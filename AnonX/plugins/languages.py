19e2ab3009b1936c4311cbfabe9325d25a10058a
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-12-09 09:38:08 +0300

    Update languages.py

--- AnonX/plugins/languages.py
+++ AnonX/plugins/languages.py
@@ -1,87 +1,147 @@
 from pykeyboard import InlineKeyboard
+
 from pyrogram import filters
+
 from pyrogram.types import InlineKeyboardButton, Message
 
-from config import BANNED_USERS
-from strings import get_command, get_string
-from AnonX import app
-from AnonX.utils.database import get_lang, set_lang
-from AnonX.utils.decorators import (ActualAdminCB, language,
-                                         languageCB)
 
-# Languages Available
+
+from AnonXMusic import app
+
+from AnonXMusic.utils.database import get_lang, set_lang
+
+from AnonXMusic.utils.decorators import ActualAdminCB, language, languageCB
+
+from config import BANNED_USERS
+
+from strings import get_string, languages_present
+
+
+
 
 
 def lanuages_keyboard(_):
+
     keyboard = InlineKeyboard(row_width=2)
-    keyboard.row(
-        InlineKeyboardButton(
-            text="العربيـة",
-            callback_data=f"languages:ar",
-        ),
+
+    keyboard.add(
+
+        *[
+
+            (
+
+                InlineKeyboardButton(
+
+                    text=languages_present[i],
+
+                    callback_data=f"languages:{i}",
+
+                )
+
+            )
+
+            for i in languages_present
+
+        ]
+
     )
+
     keyboard.row(
+
         InlineKeyboardButton(
-            text=_["CLOSE_BUTTON"], callback_data=f"close"
+
+            text=_["BACK_BUTTON"],
+
+            callback_data=f"settingsback_helper",
+
         ),
+
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
+
     )
+
     return keyboard
 
 
-LANGUAGE_COMMAND = get_command("LANGUAGE_COMMAND")
 
 
-@app.on_message(
-    filters.command(LANGUAGE_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
-)
+
+@app.on_message(filters.command(["lang", "setlang", "language"]) & ~BANNED_USERS)
+
 @language
+
 async def langs_command(client, message: Message, _):
+
     keyboard = lanuages_keyboard(_)
+
     await message.reply_text(
-        _["setting_1"].format(message.chat.title, message.chat.id),
+
+        _["lang_1"],
+
         reply_markup=keyboard,
+
     )
+
+
+
 
 
 @app.on_callback_query(filters.regex("LG") & ~BANNED_USERS)
+
 @languageCB
+
 async def lanuagecb(client, CallbackQuery, _):
+
     try:
+
         await CallbackQuery.answer()
+
     except:
+
         pass
+
     keyboard = lanuages_keyboard(_)
-    return await CallbackQuery.edit_message_reply_markup(
-        reply_markup=keyboard
-    )
+
+    return await CallbackQuery.edit_message_reply_markup(reply_markup=keyboard)
 
 
-@app.on_callback_query(
-    filters.regex(r"اللغات:(.*?)") & ~BANNED_USERS
-)
+
+
+
+@app.on_callback_query(filters.regex(r"languages:(.*?)") & ~BANNED_USERS)
+
 @ActualAdminCB
+
 async def language_markup(client, CallbackQuery, _):
+
     langauge = (CallbackQuery.data).split(":")[1]
+
     old = await get_lang(CallbackQuery.message.chat.id)
+
     if str(old) == str(langauge):
-        return await CallbackQuery.answer(
-            "ʏᴏᴜ'ʀᴇ ᴀʟʀᴇᴀᴅʏ ᴜsɪɴɢ sᴀᴍᴇ ʟᴀɴɢᴜᴀɢᴇ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ.", show_alert=True
-        )
+
+        return await CallbackQuery.answer(_["lang_4"], show_alert=True)
+
     try:
+
         _ = get_string(langauge)
-        await CallbackQuery.answer(
-            "sᴜᴄᴄᴇssғᴜʟʟʏ ᴄʜᴀɴɢᴇᴅ ʏᴏᴜʀ ʟᴀɴɢᴜᴀɢᴇ.", show_alert=True
-        )
+
+        await CallbackQuery.answer(_["lang_2"], show_alert=True)
+
     except:
+
+        _ = get_string(old)
+
         return await CallbackQuery.answer(
-            "ғᴀɪʟᴇᴅ ᴛᴏ ᴄʜᴀɴɢᴇ ʟᴀɴɢᴜᴀɢᴇ ᴏʀ ᴛʜᴇ ʟᴀɴɢᴜᴀɢᴇ ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ.",
+
+            _["lang_3"],
+
             show_alert=True,
+
         )
+
     await set_lang(CallbackQuery.message.chat.id, langauge)
+
     keyboard = lanuages_keyboard(_)
-    return await CallbackQuery.edit_message_reply_markup(
-        reply_markup=keyboard
-    )
+
+    return await CallbackQuery.edit_message_reply_markup(reply_markup=keyboard)
